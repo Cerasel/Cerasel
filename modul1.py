4a9a820342a03f4e33c1c4688bc1b265fe73f764
Cerasela <cerasela.basanete@gmail.com>
2021-04-14 21:27:20 +0300

    add module 1

---
+++ modul1.py
@@ -0,0 +1,35 @@
+print('Hello World')
+print('print\u200b("")')
+# print​("")
+HELLO_WORLD = 'Hello World'
+print(HELLO_WORLD)
+
+name = 'Cerasela'
+print(100 * (name+'\n') )
+
+print(80*'#')
+print(3 * name)
+print(name * 3)
+
+print(print)
+
+# print int
+print(3)
+print('3')
+
+print('----')
+print('|   |')
+print('|   |')
+print('----')
+
+
+
+print('Triunghi: ')
+print("   / \ ")
+print("  /   \ ")
+print(' /     \ ')
+print("/_______\ ")
+print('Patrat: ')
+print(" _____")
+print("|     |")
+print("|_____|")
