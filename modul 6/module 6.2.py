ec77bda2ed0be984217c398af52b5f1541e926c2
Cerasela <cerasela.basanete@gmail.com>
2021-06-12 10:35:54 +0300

    module5

---
+++ modul 6/module 6.2.py
@@ -0,0 +1,18 @@
+# open
+
+file = open('my_file.txt' , 'w')
+file.write('My new text')
+file.close()
+
+with open('my_file.txt' , 'w') as fils :
+    file.write('Random text')
+
+print(type(file))
+
+clas A()
+__test ='test'
+
+
+a = A()
+print(a._A__test)
+
